f2b063917a2d79148b1d1d1decd7e7a563a7a51c
Larissa Sthefany <110210572+Larissasthefany@users.noreply.github.com>
2023-01-13 18:16:28 -0300

    Alteração nas imagens

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,15 @@ $('.btn').click(function(){
 });
 
 function trocarImagem1(){
-    document.querySelector('body').style.background = "url('umbrella.jpg') center center / cover";
+    document.querySelector('body').style.background = "url('bleach.jpg') center center / cover";
 }
 
 function trocarImagem2(){
-    document.querySelector('body').style.background = "url('capa.jpg') center center / cover"
+    document.querySelector('body').style.background = "url('one-piece.png') center center / cover"
 
 }
 
 function trocarImagem3(){
-    document.querySelector('body').style.background = "url('naruto.webp') center center / cover"
+    document.querySelector('body').style.background = "url('itachi.webp') center center / cover"
 }
 
