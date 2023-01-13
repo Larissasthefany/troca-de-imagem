4b7cd3fece7a14f6340b338a3d5ba65b23fe49be
Larissa Sthefany <110210572+Larissasthefany@users.noreply.github.com>
2023-01-13 00:53:09 -0300

    Atualização

--- src/js/index.js
+++ src/js/index.js
@@ -4,15 +4,15 @@ $('.btn').click(function(){
 });
 
 function trocarImagem1(){
-    document.querySelector('body').style.background = "url('../src/img/umbrella.jpg') center center / cover";
+    document.querySelector('body').style.background = "url('umbrella.jpg') center center / cover";
 }
 
 function trocarImagem2(){
-    document.querySelector('body').style.background = "url('../src/img/capa.jpg') center center / cover"
+    document.querySelector('body').style.background = "url('capa.jpg') center center / cover"
 
 }
 
 function trocarImagem3(){
-    document.querySelector('body').style.background = "url('../src/img/naruto.webp') center center / cover"
+    document.querySelector('body').style.background = "url('naruto.webp') center center / cover"
 }
 
